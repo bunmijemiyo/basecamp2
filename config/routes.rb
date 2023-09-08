aeebce491fe643e8cfcc95aba2d01c5516ef8d44
olubunmi <bjemiyor@gmail.com>
2023-09-08 07:10:41 +0100

    added projects listing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :projects, only: [:show]
+  # root "pages#home"
+  resources :projects, only: [:show, :index]
 end
