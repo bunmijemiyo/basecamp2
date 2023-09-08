7d5686e9f02dfc51213799a154244c9517f4c577
olubunmi <bjemiyor@gmail.com>
2023-09-08 10:22:53 +0100

    Added edit and update functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,33 @@ class ProjectsController < ApplicationController
     def index
         @projects = Project.all
     end
+
+    def new
+        @project = Project.new
+    end
+
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def create
+        @project = Project.new(params.require(:project).permit(:title, :description, :image))
+        if @project.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to @project
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        if @project.update(params.require(:project).permit(:title, :description, :image))
+            flash[:notice] = "Project was updated successfully"
+            redirect_to @project
+        else
+            render 'edit'
+        end
+    end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+    has_one_attached :image
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
