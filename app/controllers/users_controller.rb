098193ab7a9bdfcb39926246069ac0f64ff8754d
olubunmi <bjemiyor@gmail.com>
2023-09-15 09:31:00 +0100

    Add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
         @projects = @user.projects
     end
 
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
